c5171ee0334b93ae1b8d0c6c89cc13f0fd7c4221
joaopedrotecnologia <99668248+joaopedrotecnologia@users.noreply.github.com>
2024-05-15 16:24:31 -0300

    Create entrypoint.sh

---
+++ build/scripts/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+printf "\n\Starting PHP $PHP_VERSION daemon... \n\n";
+php-fpm --daemonize
+
+printf "Starting Nginx...\n\n"
+set -e 
+
+if [[ "$1" == -*]] then
+    set -- nginx -g daemon off; "$@"
+fi
+
+
+printf "Running command...\n\n\"
+exec "$@"
